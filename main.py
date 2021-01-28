a306623564b9aa3a481fdc79a996adba64700bdd
ozgur <ozgur.ersoz3@gmail.com>
2021-01-28 11:33:38 +0300

    3rd column

--- main.py
+++ main.py
@@ -9,4 +9,5 @@ st.write("Here's our first attempt at using data to create a table:")
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
+    'third column': [100, 200, 300, 400]
 }))
