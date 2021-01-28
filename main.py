897a0357a7efae4221a1a91d30ac69bb6a15b469
ozgur <ozgur.ersoz3@gmail.com>
2021-01-28 11:36:39 +0300

    correction

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ st.title('My first app')
 st.write("Here's our first attempt at using data to create a table:")
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
+    'second column': [10, 20, 30, 40],
     'third column': [100, 200, 300, 400]
 }))
