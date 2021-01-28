5b337289dafe88a3038891ec748c790fccf7ed08
Ozgur Ersoz <ozgur.ersoz3@gmail.com>
2021-01-28 22:25:51 +0300

    sliders

--- main.py
+++ main.py
@@ -11,3 +11,18 @@ st.write(pd.DataFrame({
     'second column': [10, 20, 30, 40],
     'third column': [100, 200, 300, 400]
 }))
+
+
+st.button("Start")
+
+row = st.slider("Row", 0,50)
+col = st.slider("Column", 0,50)
+
+data = np.ones((row,col))
+st.write(data)
+
+
+df = pd.DataFrame(np.array([[1, 3 ,5], [7, 9, 11], [34, 545, 76]]), columns= ["a", "b", "c"])
+st.write(df)
+
+
